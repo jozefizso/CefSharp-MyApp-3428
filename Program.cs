2518775676e829de1fb3b95bd711c0172f6c2732
Jozef Izso <jozef.izso@gmail.com>
2021-03-08 12:24:30 +0100

    Add CefSharp v87

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +17,13 @@ namespace MyApp
         [STAThread]
         static void Main()
         {
+            Cef.EnableHighDPISupport();
+
+            var settings = new CefSettings();
+            settings.BrowserSubprocessPath = Path.GetFullPath(@"x86\CefSharp.BrowserSubprocess.exe");
+
+            Cef.Initialize(settings);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
